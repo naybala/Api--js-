8875a6d351f95c9910299562d5ddde21a40669fe
naybala <naybalaismylife.nbl@gmail.com>
2022-02-24 13:00:58 +0630

    adding auto complete search

--- custom.js
+++ custom.js
@@ -10,6 +10,45 @@ var apiCall = async () => {
     const jsonResponse = await response.json();
     //    console.log(jsonResponse);
     var tBody = document.getElementById('t-body');
+    const autoCompleteInputTag = document.querySelector(".autoCompleteInput", [0]);
+
+    const resultContainerTag = document.querySelector(".resultContainer", [0]);
+
+    autoCompleteInputTag.addEventListener("keyup", (e) => {
+        resultContainerTag.innerHTML = "";
+        const searchText = e.target.value.toLowerCase();
+        if (searchText.length === 0) {
+            return;
+        }
+        const filteredProducts = jsonResponse.filter((product) => {
+            return product.title.toLowerCase().includes(searchText);
+        });
+
+        if (filteredProducts.length > 0) {
+            for (let i = 0; i < filteredProducts.length; i++) {
+                const productItemContainer = document.createElement("div");
+                productItemContainer.id = filteredProducts[i].id;
+                productItemContainer.classList.add("productItemContainer");
+
+                const productId = document.createElement("div");
+                productId.classList.add("productId");
+                productId.append(filteredProducts[i].id);
+
+                const productName = document.createElement("div");
+                productName.classList.add("productName");
+                productName.append(filteredProducts[i].title);
+
+                const productImage = document.createElement("img");
+                productImage.classList.add("productImage");
+                productImage.src = filteredProducts[i].image;
+
+                productItemContainer.append(productId, productName, productImage);
+                resultContainerTag.append(productItemContainer);
+            }
+        }
+    });
+
+
     var myList = '';
 
     for (var i = 0; i < jsonResponse.length; i++) {
@@ -18,7 +57,7 @@ var apiCall = async () => {
             `<tr>
  <td>${jsonResponse[i].id}</td>
  <td>${jsonResponse[i].title}</td>
- <td></td>
+
  <td>${jsonResponse[i].price} mmk</td>
  <td><image src="${jsonResponse[i].image} " class="img"></td>
 </tr>`
